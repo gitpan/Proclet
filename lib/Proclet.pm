e014a7497ac6c8e67bc0c5d09f11bf35e4f7d0ca
Masahiro Nagano <kazeburo@gmail.com>
2012-08-31 06:31:41 +0000

    Import of KAZEBURO/Proclet-0.10 from CPAN.
    gitpan-cpan-distribution: Proclet
    gitpan-cpan-version:      0.10
    gitpan-cpan-path:         KAZEBURO/Proclet-0.10.tar.gz
    gitpan-cpan-author:       KAZEBURO
    gitpan-cpan-maturity:     released

--- lib/Proclet.pm
+++ lib/Proclet.pm
@@ -17,7 +17,7 @@ subtype 'ServiceProcs'
 
 no Mouse::Util::TypeConstraints;
 
-our $VERSION = '0.09';
+our $VERSION = '0.10';
 
 has '_services' => (
     is => 'ro',
@@ -41,6 +41,12 @@ has 'color' => (
     is => 'rw',
     isa => 'Bool',
     default => 0,
+);
+
+has 'logger' => (
+    is => 'ro',
+    isa => 'CodeRef',
+    required => 0,
 );
 
 my $rule = Data::Validator->new(
@@ -201,7 +207,11 @@ sub log_worker {
                     $prefix = colored( $prefix, $services->{$sid}->{color} ) if $self->color;
                     chomp $log;
                     chomp $log;
-                    warn  $prefix . ' ' . $log . "\n";
+                    if ( $self->logger ) {
+                        $self->logger->($prefix . ' ' . $log . "\n");
+                    } else {
+                        warn  $prefix . ' ' . $log . "\n";
+                    }
             }
             }
         }
@@ -294,6 +304,15 @@ number of seconds to deter spawning of services after a service exits abnormally
 =item color: Bool
 
 colored log (default: 0)
+
+=item logger: CodeRef
+
+  my $logger = File::RotateLogs->new(...)
+  my $proclet = Proclet->new(
+      logger => sub { $logger->print(@_) }
+  );
+  
+Sets a callback to print stdout/stderr. uses warn by default.
 
 =back
 
